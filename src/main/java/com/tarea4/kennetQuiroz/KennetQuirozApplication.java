83d0e76a1e83e0742978a1ef2d31a6ca65a0e390
Kennet Quiróz <78316880+KennetQ2903@users.noreply.github.com>
2025-08-27 22:46:33 -0600

    feat: init commit

---
+++ src/main/java/com/tarea4/kennetQuiroz/KennetQuirozApplication.java
@@ -0,0 +1,13 @@
+package com.tarea4.kennetQuiroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennetQuirozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KennetQuirozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea4/kennetQuiroz/KennetQuirozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea4.kennetQuiroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KennetQuirozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
